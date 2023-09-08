c2b4e3854db98ded34b40d507d071a496793a566
arthur.vassoler <arthur.vassoler@meta.com.br>
2023-09-08 17:18:09 -0300

    first commit

---
+++ src/app/shared/card/card.component.ts
@@ -0,0 +1,10 @@
+import { Component, INJECTOR, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent {
+  @Input() variant: 'primary' | 'secondary' = 'primary'
+}
